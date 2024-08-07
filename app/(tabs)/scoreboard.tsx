62f6a6f690502d931fd095af43fcd72688433b06
Phirks <Phirks@outlook.com>
2024-08-06 21:27:54 -0400

    converted data type to decimal from remote device

--- app/(tabs)/scoreboard.tsx
+++ app/(tabs)/scoreboard.tsx
@@ -1,6 +1,6 @@
 import { View, Text, SafeAreaView } from 'react-native'
 import React from 'react'
-import { useGlobalContext } from "../../context/GlobalProvider";
+import { useGlobalContext, monitorSetup } from "../../context/GlobalProvider";
 import CustomButton from "../../components/CustomButton"
 import { BleManager, Device } from 'react-native-ble-plx'
 import { useState } from "react";
@@ -11,52 +11,62 @@ const charactaristicRX = (error, characteristic) => {
   console.log(characteristic)
 }
 const Scoreboard = () => {
-  const { BLEService2,connectedDevice } = useGlobalContext();
+  const { BLEService2, connectedDevice } = useGlobalContext();
 
-  
+
 
   return (
-    <SafeAreaView className='bg-primary h-full w-full flex-row items-end'>
-      <View className="space-y-3 pb-5 flex-grow">
+    <SafeAreaView className='bg-primary mt-6 h-full w-full items-start'>
+      <View className='items-center w-full'>
       <CustomButton
-          title="+"
-          handlePress={() => {
-            BLEService2.manager.monitorCharacteristicForDevice(connectedDevice.id,"6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400003-b5a3-f393-e0a9-e50e24dcca9e",(error, characteristic) => {console.log(base64.decode(characteristic.value))})
-            connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "AA==")
-          }}
-          containerStyles='mt-7 mx-4 bg-red-500'
-          textStyles={'text-3xl'}
-        />
-        
-        <CustomButton
-          title="-"
-          handlePress={() => {
-            connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "AQ==")
-          }}
-          containerStyles='mt-7 mx-4 bg-red-500'
-          textStyles={'text-3xl'}
-        />
+            title=" Setup Connection "
+            handlePress={() => {
+              monitorSetup(BLEService2.manager,connectedDevice.id)
+            }}
+            containerStyles='mt-7 mx-4 bg-red-500'
+            textStyles={'text-3xl'}
+          />
+      </View>
+      <View className='h-full w-full flex-row items-start'>
+        <View className="space-y-3 pb-5 flex-grow">
+          <CustomButton
+            title="+"
+            handlePress={() => {
+              connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "AA==")
+            }}
+            containerStyles='mt-7 mx-4 bg-red-500'
+            textStyles={'text-3xl'}
+          />
+
+          <CustomButton
+            title="-"
+            handlePress={() => {
+              connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "AQ==")
+            }}
+            containerStyles='mt-7 mx-4 bg-red-500'
+            textStyles={'text-3xl'}
+          />
         </View>
-      <View className="space-y-3 pb-5 flex-grow">
-      <CustomButton
-          title="+"
-          handlePress={() => {
-            connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "Ag==")
-          }}
-          containerStyles='mt-7 mx-4 bg-blue-500'
-          textStyles={'text-3xl'}
-        />
-        <CustomButton
-          title="-"
-          handlePress={() => {
-            connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "Aw==")
-          }}
-          containerStyles='mt-7 mx-4 bg-blue-500'
-          textStyles={'text-3xl'}
-        />
+        <View className="space-y-3 pb-5 flex-grow">
+          <CustomButton
+            title="+"
+            handlePress={() => {
+              connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "Ag==")
+            }}
+            containerStyles='mt-7 mx-4 bg-blue-500'
+            textStyles={'text-3xl'}
+          />
+          <CustomButton
+            title="-"
+            handlePress={() => {
+              connectedDevice.writeCharacteristicWithoutResponseForService("6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400002-b5a3-f393-e0a9-e50e24dcca9e", "Aw==")
+            }}
+            containerStyles='mt-7 mx-4 bg-blue-500'
+            textStyles={'text-3xl'}
+          />
         </View>
+      </View>
     </SafeAreaView>
-    
   )
 }
 
--- context/GlobalProvider.tsx
+++ context/GlobalProvider.tsx
@@ -3,12 +3,24 @@ import { createContext, useContext, useState, useEffect } from "react";
 import { getCurrentUser } from '../lib/appwrite'
 import { BLEService } from '../services/index'
 import { BleManager, Characteristic, Device } from "react-native-ble-plx";
+import base64 from 'react-native-base64'
 
 const GlobalContext = createContext();
 export const useGlobalContext = () => useContext(GlobalContext);
 
+function convertStringToByteArray(str) {                                                                                                                                      
+    var bytes = [];                                                                                                                                                             
+    for (var i = 0; i < str.length; ++i) {                                                                                                                                      
+      bytes.push(str.charCodeAt(i));                                                                                                                                            
+    }                                                                                                                                                                           
+    return bytes                                                                                                                                                                
+  }
 
-
+export const monitorSetup = async(BLEManager: any,deviceId: string) => {
+    return await BLEManager.monitorCharacteristicForDevice(deviceId,"6e400001-b5a3-f393-e0a9-e50e24dcca9e", "6e400003-b5a3-f393-e0a9-e50e24dcca9e",(error, characteristic) => {
+        console.log(convertStringToByteArray(base64.decode(characteristic.value))[0])
+    })
+}
 
 
 
@@ -21,6 +33,7 @@ const GlobalProvider = ({ children }) => {
     const [isLoading, setIsLoading] = useState(true);
     
     useEffect(() => {
+        setConnectedDevice(BLEService2.getDevice())
         if(connectedDevice===null){
             setIsConnected(false)
         }
